4385e680abab91c5465068da859df67e005a4542
htochko <lentochko@gmail.com>
2022-09-26 16:13:53 +0200

    front-end configuration

---
+++ assets/components/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2 class="center">Schedule Application</h2>
+    {{ message }}
+    <ul>
+      <li :key="word.id" v-for="word in words">{{ word }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: "list of stops",
+      words: []
+    };
+  },
+  mounted() {
+
+    //let el = document.querySelector("div[data-words]");
+    let mywords = ['T12', '0', '1'];//el.dataset.words.split(",");
+
+    this.words.push.apply(this.words, mywords);
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,10 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import Vue from 'vue';
+import App from './components/App';
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+});
