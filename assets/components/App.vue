0d6d351101373d6b4431d60d891c9ccf07b50ccb
htochko <lentochko@gmail.com>
2022-09-27 11:52:46 +0200

    vue based stop's select

--- assets/components/App.vue
+++ assets/components/App.vue
@@ -1,32 +1,93 @@
 <template>
   <div>
     <h2 class="center">Schedule Application</h2>
-    {{ message }}
-    <ul>
-      <li :key="word.id" v-for="word in words">{{ word }}</li>
-    </ul>
+    <div class='main-wrapper'>
+      <div class='row'>
+        <div class='column'>
+          {{ message }}
+          <v-select label="name"
+                    :filterable="false"
+                    :options="options"
+                    @search="onSearch"
+                    :reduce="name => name.name"
+          >
+            <template slot="no-options">
+              type to search stops..
+            </template>
+            <template slot="option" slot-scope="option">
+              <div class="d-center">
+                {{ option.name }}
+              </div>
+            </template>
+            <template slot="selected-option" slot-scope="option">
+              <div class="selected d-center">
+                {{ option.name }}
+              </div>
+            </template>
+          </v-select>
+        </div>
+        <div class='column'>
+          Nearest trips: <!-- Selected  -->
+          <!-- table of Nearest routes -->
+        </div>
+      </div>
+    </div>
+
+
+
   </div>
 </template>
 
 <script>
+import _ from 'lodash';
+import Vue from 'vue';
+import vSelect from 'vue-select';
+import 'vue-select/dist/vue-select.css';
+Vue.component('v-select', vSelect);
+
 export default {
   name: 'App',
   data() {
     return {
       message: "list of stops",
-      words: []
+      options: [],
     };
   },
+  methods: {
+    onSearch(search, loading) {
+      if(search.length) {
+        loading(true);
+        this.search(loading, search, this);
+      }
+    },
+    search: _.debounce((loading, search, vm) => {
+      fetch(
+          `http://localhost:8080/api/stops?page=1&name=${escape(search)}`
+      ).then(res => {
+        res.json().then(json => (vm.options = json['hydra:member']));
+        loading(false);
+      });
+    }, 350)
+  },
   mounted() {
-
-    //let el = document.querySelector("div[data-words]");
-    let mywords = ['T12', '0', '1'];//el.dataset.words.split(",");
-
-    this.words.push.apply(this.words, mywords);
+    // todo add initial list from real stops
   }
 };
 </script>
 
 <style scoped>
+.row {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+}
 
+.column {
+  display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  flex: 1;
+  border: 2px solid #5e2ca5;
+}
 </style>
--- src/Entity/Stop.php
+++ src/Entity/Stop.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\StopRepository;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -10,6 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 #[ApiResource]
 #[ORM\Entity(repositoryClass: StopRepository::class)]
+#[ApiFilter(SearchFilter::class, properties: ['name' => 'partial'])]
 class Stop
 {
     #[ORM\Id]
